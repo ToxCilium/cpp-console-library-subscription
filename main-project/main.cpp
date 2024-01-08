dc3f1080b2533e83353d8b3942b84938f91ff70e
Cilum <sereda_maksimka@bk.ru>
2024-01-08 05:46:46 +0300

    Проверка2

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -3,51 +3,51 @@
 
 using namespace std;
 
-//#include "catalog.h"
+//#include "book_subscription.h"
 //#include "file_reader.h"
 //#include "constants.h"
 //#include "filter.h"
 //#include "processing.h"
 //
-//void output(catalog* items)
+//void output(book_subscription* subscription)
 //{
-//	/********** âûâîä ÷èòàòåëÿ **********/
-//	cout << "×èòàòåëü........: ";
-//	// âûâîä ôàìèëèè
+//	/********** вывод читателя **********/
+//	cout << "Читатель........: ";
+//	// вывод фамилии
 //	cout << subscription->reader.last_name << " ";
-//	// âûâîä ïåðâîé áóêâû èìåíè
+//	// вывод первой буквы имени
 //	cout << subscription->reader.first_name[0] << ". ";
-//	// âûâîä ïåðâîé áóêâû îò÷åñòâà
+//	// вывод первой буквы отчества
 //	cout << subscription->reader.middle_name[0] << ".";
 //	cout << '\n';
-//	/********** âûâîä êíèãè **********/
-//	cout << "Êíèãà...........: ";
-//	// âûâîä ôàìèëèè àâòîðà
+//	/********** вывод книги **********/
+//	cout << "Книга...........: ";
+//	// вывод фамилии автора
 //	cout << subscription->author.last_name << " ";
-//	// âûâîä ïåðâîé áóêâû èìåíè àâòîðà
+//	// вывод первой буквы имени автора
 //	cout << subscription->author.first_name[0] << ". ";
-//	// âûâîä ïåðâîé áóêâû îò÷åñòâà àâòîðà
+//	// вывод первой буквы отчества автора
 //	cout << subscription->author.middle_name[0] << ".";
 //	cout << ", ";
-//	// âûâîä íàçâàíèÿ
+//	// вывод названия
 //	cout << '"' << subscription->title << '"';
 //	cout << '\n';
-//	/********** âûâîä äàòû âûäà÷è **********/
-//	// âûâîä ãîäà
-//	cout << "Äàòà âûäà÷è.....: ";
+//	/********** вывод даты выдачи **********/
+//	// вывод года
+//	cout << "Дата выдачи.....: ";
 //	cout << setw(4) << setfill('0') << subscription->start.year << '-';
-//	// âûâîä ìåñÿöà
+//	// вывод месяца
 //	cout << setw(2) << setfill('0') << subscription->start.month << '-';
-//	// âûâîä ÷èñëà
+//	// вывод числа
 //	cout << setw(2) << setfill('0') << subscription->start.day;
 //	cout << '\n';
-//	/********** âûâîä äàòû âîçâðàòà **********/
-//	// âûâîä ãîäà
-//	cout << "Äàòà âîçâðàòà...: ";
+//	/********** вывод даты возврата **********/
+//	// вывод года
+//	cout << "Дата возврата...: ";
 //	cout << setw(4) << setfill('0') << subscription->finish.year << '-';
-//	// âûâîä ìåñÿöà
+//	// вывод месяца
 //	cout << setw(2) << setfill('0') << subscription->finish.month << '-';
-//	// âûâîä ÷èñëà
+//	// вывод числа
 //	cout << setw(2) << setfill('0') << subscription->finish.day;
 //	cout << '\n';
 //	cout << '\n';
@@ -56,61 +56,8 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << "Ëàáîðàòîðíàÿ ðàáîòà ¹1. GITi\n";
-	cout << "Âàðèàíò ¹10. Êàòàëîã òîâàðîâ\n";
-	cout << "Àâòîð: Ìàêñèì Ñåðåäà\n";
-	cout << "Ãðóïïà: 23ÏÈíæ1ç\n\n";
-	//catalog* items[MAX_FILE_ROWS_COUNT];
-	//int size;
-	//try
-	//{
-	//	read("data.txt", items, size);
-	//	cout << "***** Áèáëèîòå÷íûé àáîíåìåíò *****\n\n";
-	//	for (int i = 0; i < size; i++)
-	//	{
-	//		output(items[i]);
-	//	}
-	//	bool (*check_function)(catalog*) = NULL; // check_function - ýòî óêàçàòåëü íà ôóíêöèþ, âîçâðàùàþùóþ çíà÷åíèå òèïà bool,
-	//													   // è ïðèíèìàþùóþ â êà÷åñòâå ïàðàìåòðà çíà÷åíèå òèïà catalog*
-	//	cout << "\nÂûáåðèòå ñïîñîá ôèëüòðàöèè èëè îáðàáîòêè äàííûõ:\n";
-	//	cout << "1) Òîâàðû êàòåãîðèè ïðîìòîâàðû \n";
-	//	cout << "2) Òîâàðû ñòîèìîñòüþ áîëüøå 100 ðóáëåé\n";
-	//	cout << "\nÂâåäèòå íîìåð âûáðàííîãî ïóíêòà: ";
-	//	int item;
-	//	cin >> item;	
-	//	cout << '\n';
-	//	switch (item)
-	//	{
-	//	case 1:
-	//		check_function = check_catalog_by_category; // ïðèñâàèâàåì â óêàçàòåëü íà ôóíêöèþ ñîîòâåòñòâóþùóþ ôóíêöèþ
-	//		cout << "***** Òîâàðû êàòåãîðèè ïðîìòîâàðû *****\n\n";
-	//		break;
-	//	case 2:
-	//		check_function = check_book_subscription_by_date; // ïðèñâàèâàåì â óêàçàòåëü íà ôóíêöèþ ñîîòâåòñòâóþùóþ ôóíêöèþ
-	//		cout << "***** Òîâàðû ñî ñòîèìîñòüþ áîëüøå 100 ðóáëåé *****\n\n";
-	//		break;
-	//	
-	//	default:
-	//		throw "Íåêîððåêòíûé íîìåð ïóíêòà";
-	//	}
-	//	if (check_function)
-	//	{
-	//		int new_size;
-	//		catalog** filtered = filter(subscriptions, size, check_function, new_size);
-	//		for (int i = 0; i < new_size; i++)
-	//		{
-	//			output(filtered[i]);
-	//		}
-	//		delete[] filtered;
-	//	}
-	//	for (int i = 0; i < size; i++)
-	//	{
-	//		delete subscriptions[i];
-	//	}
-	//}
-	//catch (const char* error)
-	//{
-	//	cout << error << '\n';
-	//}
-	//return 0;
+	cout << "Лабораторная работа №1. GIT\n";
+	cout << "Вариант №10. Католог товаров\n";
+	cout << "Автор: Середа максим\n";
+	cout << "Группа: 23ПИнж1з\n\n";
 }
